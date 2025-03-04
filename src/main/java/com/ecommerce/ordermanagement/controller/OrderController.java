f4e082c87b975bd8df7ba61230bbbdb11b72cebd
mynam durga prasad <138565993+dpmynam123@users.noreply.github.com>
2025-03-04 17:45:01 +0530

    Update OrderController.java

--- src/main/java/com/ecommerce/ordermanagement/controller/OrderController.java
+++ src/main/java/com/ecommerce/ordermanagement/controller/OrderController.java
@@ -11,7 +11,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/orders")
-@CrossOrigin("*")
+@CrossOrigin(origins = "http://localhost:4200", allowCredentials = "true")
 public class OrderController {
 
     @Autowired
